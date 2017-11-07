a6e5b009446427c258705294f7c7dd9274081890
Tianshu <zhutianshuuoft@gmail.com>
2017-11-07 14:11:52 -0500

    working on q2

--- a2/table_definitions/q1.sql
+++ a2/table_definitions/q1.sql
@@ -15,7 +15,7 @@ partyName VARCHAR(100)
 
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS year_countryName, voteRange_partyName CASCADE;
 
 -- Define views for your intermediate steps here.
 create view year_countryName as(
@@ -24,24 +24,24 @@ create view year_countryName as(
         country.name as countryName
     from country, election
     where country.id = election.country_id and
-        extract(year from election.edate) >= 1996 and
-        extract(year from election.edate) <= 2016
+        extract(year from election.e_date) >= 1996 and
+        extract(year from election.e_date) <= 2016
 );
 
 create view voteRange_partyName as(
-    select election.id as election_id, party.name as partyName,
+    select election.id as election_id, party.name_short as partyName,
         case
-        when 0 <= election_result.votes/elction.votes_valid and election_result.votes/elction.votes_valid <= 5
+        when 0 <= 1.0*election_result.votes/election.votes_valid and 1.0*election_result.votes/election.votes_valid <= 0.05
         Then '(0,5]'
-        when 5 < election_result.votes/elction.votes_valid and election_result.votes/elction.votes_valid <= 10
+        when 0.05 < 1.0*election_result.votes/election.votes_valid and 1.0*election_result.votes/election.votes_valid <= 0.1
         Then '(5,10]'
-        when 10 < election_result.votes/elction.votes_valid and election_result.votes/elction.votes_valid <= 20
+        when 0.1 < 1.0*election_result.votes/election.votes_valid and 1.0*election_result.votes/election.votes_valid <= 0.2
         Then '(10,20]'
-        when 20 < election_result.votes/elction.votes_valid and election_result.votes/elction.votes_valid <= 30
+        when 0.2 < 1.0*election_result.votes/election.votes_valid and 1.0*election_result.votes/election.votes_valid <= 0.3
         Then '(20,30]'
-        when 30 < election_result.votes/elction.votes_valid and election_result.votes/elction.votes_valid <= 40
+        when 0.3 < 1.0*election_result.votes/election.votes_valid and 1.0*election_result.votes/election.votes_valid <= 0.4
         Then '(30,40]'
-        when 40 < election_result.votes/elction.votes_valid and election_result.votes/elction.votes_valid <= 100
+        when 0.4 < 1.0*election_result.votes/election.votes_valid and 1.0*election_result.votes/election.votes_valid <= 1
         Then '(40,100]'
         end as voteRange
     from election, election_result, party
--- a2/table_definitions/q1_order.sql
+++ a2/table_definitions/q1_order.sql
@@ -1,3 +1,5 @@
+SET SEARCH_PATH TO parlgov;
+
 select *
 from q1
 order by year desc, countryName desc, voteRange desc, partyName desc;
